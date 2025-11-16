85166203563844e5334cd9366fe0d963ad2d79ee
limbopro <service.limbopro.xyz@gmail.com>
2025-11-16 16:24:09 +0800

    jwksm

--- jwksm/yaml.js
+++
@@ -1,9 +0,0 @@
-const codeBlocks = document.querySelectorAll('code.hljs.yaml');
-const keywords = ['交换', '夫妻']; // Add more keywords here as needed
-let allText = '';
-codeBlocks.forEach(block => {
-    allText += block.innerText + '\n';
-});
-const lines = allText.split('\n');
-const matches = lines.filter(line => keywords.some(keyword => line.includes(keyword)));
-console.log(matches.join('\n'));
